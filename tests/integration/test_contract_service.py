1f29e3b16c1b7df06985adf0391ae906f614cd31
Max Kobozev <quesesr@gmail.com>
2024-08-14 01:39:21 +0400

    add more contract tests

--- tests/integration/test_contract_service.py
+++ tests/integration/test_contract_service.py
@@ -36,20 +36,31 @@ class TestContractService:
         )
         self.contract_1 = Contract(
             terms="Test contract",
-            status=ContractStatus.new,
             client=self.client_1,
             contractor=self.contractor_1,
         )
+        self.contract_2_in_progress = Contract(
+            terms="Test contract",
+            client=self.client_1,
+            contractor=self.contractor_1,
+        )
+        self.contract_2_in_progress.status = ContractStatus.in_progress
+        self.contract_3_terminated = Contract(
+            terms="Test contract",
+            client=self.client_1,
+            contractor=self.contractor_1,
+        )
+        self.contract_3_terminated.status = ContractStatus.terminated
 
     def test_get_by_id(self):
         self.session.add_all([self.client_1, self.contractor_1, self.contract_1])
         self.session.flush()
 
-        contract_as_client = self.service.get_by_id(self.contract_1.id, self.client_1.id)
-        assert contract_as_client == self.contract_1
+        res_client = self.service.get_by_id(self.contract_1.id, self.client_1.id)
+        assert res_client == self.contract_1
 
-        contract_as_contractor = self.service.get_by_id(self.contract_1.id, self.contractor_1.id)
-        assert contract_as_contractor == self.contract_1
+        res_contractor = self.service.get_by_id(self.contract_1.id, self.contractor_1.id)
+        assert res_contractor == self.contract_1
 
     def test_get_by_id_unauthorized(self):
         self.session.add_all([self.client_1, self.contractor_1, self.contract_1, self.client_2])
@@ -63,3 +74,32 @@ class TestContractService:
             self.service.get_by_id(self.contract_1.id, self.contractor_2.id)
         except AppException as e:
             assert e.code == 404
+
+    def test_list_active(self):
+        self.session.add_all(
+            [
+                self.client_1,
+                self.contractor_1,
+                self.contract_1,
+                self.contract_2_in_progress,
+                self.contract_3_terminated,
+            ]
+        )
+        self.session.flush()
+
+        res = self.service.list_active(self.client_1.id)
+        assert len(res) == 2
+        assert self.contract_1 in res
+        assert self.contract_2_in_progress in res
+
+    def test_list_active_unauthorized(self):
+        self.session.add_all(
+            [self.client_1, self.contractor_1, self.contract_1, self.client_2, self.contractor_2]
+        )
+        self.session.flush()
+
+        res_client = self.service.list_active(self.client_2.id)
+        assert len(res_client) == 0
+
+        res_contractor = self.service.list_active(self.contractor_2.id)
+        assert len(res_contractor) == 0
